92817b3da8193a1a2b32c311ad5ffed3f838e2a1
Vik <jzenterprize90@gmail.com>
2021-10-08 15:01:54 -0400

    Card picker

---
+++ PickRandomCards/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace PickRandomCards
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CardGame();
+        }
+        public static void CardGame()
+        {
+            Console.Write("Enter the number of cards to pick: ");
+            string userInput = Console.ReadLine();
+            if (int.TryParse(userInput, out int numberOfCards))
+            {
+                foreach (string card in CardPicker.PickSomeCards(numberOfCards))
+                {
+                    Console.WriteLine(card);
+                }
+                // The foreach executes CWL(card) for each element in the array returned by PickSomeCards
+            }
+            else
+            {
+                Console.WriteLine("Please enter a valid number ");
+                CardGame();
+            }
+        }
+    }
+}
